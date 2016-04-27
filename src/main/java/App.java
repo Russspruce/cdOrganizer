5851fe6dc7a710cf027306522ba0e29ae0d1ccfc
Andrew Russo and DJ Roychoudhury <andrew@>
2016-04-27 14:55:16 -0700

    pass rootTest, add front end.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,48 +1,28 @@
-// import java.util.ArrayList;
-// import java.util.Map;
-// import java.util.HashMap;
-// import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
-// import static spark.Spark.*;
-//
-// public class App {
-//   public static void main(String[] args) {
-//     staticFileLocation("/public");
-//     String layout = "templates/layout.vtl";
-//
-//     get("/", (request, response) -> {
-//       Map<String, Object> model = new HashMap<String, Object>();
-//       model.put("template", "templates/index.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     get("tasks/new", (request, response) -> {
-//       HashMap<String, Object> model = new HashMap<String, Object>();
-//       model.put("template", "templates/task-form.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     get("/tasks", (request, response) -> {
-//       HashMap<String, Object> model = new HashMap<String, Object>();
-//       model.put("tasks", Task.all());
-//       model.put("template", "templates/tasks.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     get("/tasks/:id", (request, response) -> {
-//       HashMap<String, Object> model = new HashMap<String, Object>();
-//       Task task = Task.find(Integer.parseInt(request.params(":id")));
-//       model.put("task", task);
-//       model.put("template", "templates/task.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     post("/tasks", (request, response) -> {
-//       HashMap<String, Object> model = new HashMap<String, Object>();
-//       String description = request.queryParams("description");
-//       Task newTask = new Task(description);
-//       model.put("template", "templates/success.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//   }
-// }
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/compactdisks", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String title = request.queryParams("title");
+      String artist = request.queryParams("artist");
+      CompactDisk newCompactDisk = new CompactDisk(title, artist);
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
+}
---
+++ src/main/java/Artists.java
@@ -0,0 +1,11 @@
+// public class Artists {
+//   private CompactDisk mAlbum;
+//
+//   public Artists(CompactDisk compactdisk) {
+//     mAlbum = compactdisk;
+//   }
+//
+//   public String {
+//
+//   }
+// }
